6a35ab491f90b8240c6dd75c6ee227090655086e
Mark Triggs <mark@dishevelled.net>
2019-05-15 10:49:11 +1000

    Remove unused bits and move MAP indexing from series_system into here

--- indexer/qsa_map_indexer.rb
+++ indexer/qsa_map_indexer.rb
@@ -3,8 +3,8 @@
 if defined?(PeriodicIndexer)
   class QSAMAPIndexer < PeriodicIndexer
 
-    EXTRA_RESOLVES = [
-    ]
+    # Current unused
+    EXTRA_RESOLVES = []
 
     SKIP_RECORD_TYPES = [
       :digital_object,
@@ -12,13 +12,6 @@ if defined?(PeriodicIndexer)
       :classification,
       :classification_term,
     ]
-
-
-    def initialize(backend = nil, state = nil, name = nil, verbose = true, config = {})
-      super
-
-      @unpublished_records = java.util.Collections.synchronizedList(java.util.ArrayList.new)
-    end
 
 
     def resolved_attributes
@@ -34,9 +27,6 @@ if defined?(PeriodicIndexer)
     def configure_doc_rules
       super
 
-      record_has_children('resource')
-      record_has_children('archival_object')
-
       add_document_prepare_hook {|doc, record|
         if doc['primary_type'] == 'agent_corporate_entity'
           doc['id'] = "agent_corporate_entity:#{doc['id'].split('/')[-1]}"
@@ -45,49 +35,21 @@ if defined?(PeriodicIndexer)
           doc['keywords'] = doc['title']
           doc['record_type'] = 'agent_corporate_entity'
         end
+
+        add_document_prepare_hook do |doc, record|
+          if record['record'].has_key?('responsible_agency')
+            doc['responsible_agency_u_sstr'] = record['record']['responsible_agency']['ref']
+          end
+
+          if record['record'].has_key?('recent_responsible_agencies')
+            doc['recent_responsible_agencies_u_sstr'] = record['record']['recent_responsible_agencies'].map{|r| r['ref']}
+          end
+
+          if record['record'].has_key?('other_responsible_agencies')
+            doc['other_responsible_agencies_u_sstr'] = record['record']['other_responsible_agencies'].map{|r| r['ref']}
+          end
+        end
       }
-
-      #     {
-      #       "id" => "agent_corporate_entity:#{row[:id]}",
-      #       "aspace_id" => row[:id].to_s,
-      #       "display_string" => row[:sort_name],
-      #       "keywords" => row[:sort_name],
-      #       "record_type" => "agent_corporate_entity",
-      #     }
-
-
-    end
-
-
-    def skip_index_record?(record)
-      published = record['record']['publish']
-
-      stage_unpublished_for_deletion(record['record']['uri']) unless published
-
-      !published
-      false
-    end
-
-
-    def skip_index_doc?(doc)
-      published = doc['publish']
-
-      stage_unpublished_for_deletion(doc['id']) unless published
-
-      !published
-      false
-    end
-
-
-    def index_round_complete(repository)
-      # Delete any unpublished records and decendents
-      #    delete_records(@unpublished_records, :parent_id_field => 'parent_id')
-      @unpublished_records.clear()
-    end
-
-
-    def stage_unpublished_for_deletion(doc_id)
-      @unpublished_records.add(doc_id)
     end
   end
 end
