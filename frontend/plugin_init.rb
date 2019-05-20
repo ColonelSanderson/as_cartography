533e3410ce0daa42592fd4524519068107245b5f
Mark Triggs <mark@dishevelled.net>
2019-05-20 16:55:02 +1000

    Improve display of browse screen

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,6 +1,9 @@
 ArchivesSpace::Application.extend_aspace_routes(File.join(File.dirname(__FILE__), "routes.rb"))
 
 Rails.application.config.after_initialize do
+  Plugins.add_facet_group_i18n("controlling_record_u_sstr",
+                               proc {|facet| "search_results.controlling_record.current" })
+
   # Eager load all JSON schemas
   Dir.glob(File.join(File.dirname(__FILE__), "..", "schemas", "*.rb")).each do |schema|
     JSONModel(File.basename(schema, ".rb").intern)
