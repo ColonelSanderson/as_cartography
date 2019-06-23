0a62af1525d7d49c566e59a0195d79cd68d47424
Mark Triggs <mark@dishevelled.net>
2019-06-24 09:56:31 +1000

    Wrap entire import in a transaction
    Also needed to manually clean the transfer JSON to remove read-only
    create_time to keep the MAP happy

--- backend/model/transfer_import_job.rb
+++ backend/model/transfer_import_job.rb
@@ -5,28 +5,33 @@ class TransferImportJob < BatchImportRunner
                         :cancel_permissions => :cancel_importer_job)
 
   def run
-    super
+    # The import will open its own transaction, but wrap the whole thing to make
+    # sure we deal with any failures by rolling back the rest of the import.
+    DB.open(true) do
+      super
 
-    begin
-      # If we succeeded, update the transfer to reflect the import status.
-      transfer_id = @json.job.fetch('opts', {}).fetch('transfer_id', nil)
+      begin
+        # If we succeeded, update the transfer to reflect the import status.
+        transfer_id = @json.job.fetch('opts', {}).fetch('transfer_id', nil)
 
-      return unless transfer_id
+        return unless transfer_id
 
-      RequestContext.open(:current_username => @job.owner.username,
-                          :repo_id => @job.repo_id) do
-        transfer_obj = Transfer[transfer_id]
-        transfer_json = Transfer.sequel_to_jsonmodel([transfer_obj])[0]
+        RequestContext.open(:current_username => @job.owner.username,
+                            :repo_id => @job.repo_id) do
+          transfer_obj = Transfer[transfer_id]
+          transfer_json = Transfer.sequel_to_jsonmodel([transfer_obj])[0]
 
-        transfer_json.checklist_metadata_imported = true
-        transfer_obj.update_from_json(transfer_json)
+          transfer_json.checklist_metadata_imported = true
+
+          cleaned = JSONModel::JSONModel(:transfer).from_hash(transfer_json.to_hash)
+          transfer_obj.update_from_json(cleaned)
+        end
+      rescue
+        Log.error("Unexpected failure in Transfer import: #{$!}")
+        Log.exception($!)
+
+        raise $!
       end
-    rescue
-      Log.error("Unexpected failure in Transfer import: #{$!}")
-      Log.exception($!)
-
-      raise $!
     end
-
   end
 end
