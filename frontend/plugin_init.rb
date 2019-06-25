1d1c5ba143d748e499450000a24b7a535fa8e48e
James Bullen <jambun@gmail.com>
2019-06-25 16:38:18 +1000

    Don't tickle _exts

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -6,6 +6,7 @@ Rails.application.config.after_initialize do
 
   # Eager load all JSON schemas
   Dir.glob(File.join(File.dirname(__FILE__), "..", "schemas", "*.rb")).each do |schema|
+    next if schema.end_with?('_ext.rb')
     JSONModel(File.basename(schema, ".rb").intern)
   end
 
