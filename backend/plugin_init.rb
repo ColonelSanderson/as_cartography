6c3c901e55b8ee257a55e4d878ba347395cf4e82
James Bullen <jambun@gmail.com>
2019-06-28 14:07:39 +1000

    Reigster MAP models for history

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,3 +1,20 @@
+# register models for history
+begin
+  [
+   Transfer,
+   TransferProposal,
+   FileIssue,
+   FileIssueRequest,
+   SearchRequest
+  ].each do |model|
+    History.register_model(model)
+    History.add_model_map(model, :last_modified_by => :modified_by,
+                                 :user_mtime => proc {|obj| Time.at(obj.modified_time/1000)})
+  end
+rescue NameError
+  Log.info("Unable to register Mandate and Function for history. Please install the as_history plugin")
+end
+
 MAPDB.connect
 
 require_relative 'index_feed/index_feed_thread'
