0f25d4280d6feb1559135c54d21385a8b4bb609c
James Bullen <jambun@gmail.com>
2019-07-05 16:47:05 +1000

    Tweak rules and include source ref

--- backend/model/validator.rb
+++ backend/model/validator.rb
@@ -37,44 +37,55 @@ class Validator
         agency_id = Integer(col)
 
         if AgentCorporateEntity[agency_id].nil?
-          notifications.add_notification(:ERROR, "Creating agency references a non-existent agency: #{agency_id}")
+          notifications.add_notification(:ERROR, "Creating agency references a non-existent agency: #{agency_id}",
+                                         @validator.formatSource(meta))
         end
       rescue ArgumentError
-        notifications.add_notification(:ERROR, "Creating Agency should contain the QSA ID of an agency (e.g. a number like 123)")
+        notifications.add_notification(:ERROR, "Creating Agency should contain the QSA ID of an agency (e.g. a number like 123)",
+                                       @validator.formatSource(meta))
       end
     }
 
     validations[:'Responsible Agency'] = lambda {|notifications, meta, col|
-      if col.to_s.strip.empty?
-        notifications.add_notification(:ERROR, "Responsible Agency is required but was missing")
-        return
-      end
+      # This field is optional
+      return if col.to_s.strip.empty?
 
       begin
         agency_id = Integer(col)
 
         if AgentCorporateEntity[agency_id].nil?
-          notifications.add_notification(:ERROR, "Responsible agency references a non-existent agency: #{agency_id}")
+          notifications.add_notification(:ERROR, "Responsible agency references a non-existent agency: #{agency_id}",
+                                         @validator.formatSource(meta))
         end
       rescue ArgumentError
-        notifications.add_notification(:ERROR, "Responsible Agency should contain the QSA ID of an agency (e.g. a number like 123)")
+        notifications.add_notification(:ERROR, "Responsible Agency should contain the QSA ID of an agency (e.g. a number like 123)",
+                                       @validator.formatSource(meta))
       end
     }
 
     validations[:'Series ID'] = lambda {|notifications, meta, col|
-      if col.to_s.strip.empty?
-        notifications.add_notification(:ERROR, "Series ID is required but was missing")
+      # series id is not required on attachment rows
+      is_attachment = !!meta[:row_fields][meta[:headers].index("Attachment Related to Sequence Number")]
+
+      if is_attachment
         return
+      else
+        if col.to_s.strip.empty?
+          notifications.add_notification(:ERROR, "Series ID is required but was missing", @validator.formatSource(meta))
+          return
+        end
       end
 
       begin
         series_id = Integer(col)
 
         if Resource[:id => series_id, :repo_id => repo_id].nil?
-          notifications.add_notification(:ERROR, "Series ID references a non-existent series: #{series_id}")
+          notifications.add_notification(:ERROR, "Series ID references a non-existent series: #{series_id}",
+                                         @validator.formatSource(meta))
         end
       rescue ArgumentError
-        notifications.add_notification(:ERROR, "Series ID must contain the QSA ID of a series (e.g. a number like 123)")
+        notifications.add_notification(:ERROR, "Series ID must contain the QSA ID of a series (e.g. a number like 123)",
+                                       @validator.formatSource(meta))
       end
     }
 
