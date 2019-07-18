83060ad3dfa916ddbc7f24c6e8bcb04dc4062a42
James Bullen <jambun@gmail.com>
2019-07-18 17:59:54 +1000

    Fixed map browse sorting

--- indexer/aspace_indexer.rb
+++ indexer/aspace_indexer.rb
@@ -26,53 +26,50 @@ class IndexerCommon
     }
 
     indexer.add_document_prepare_hook {|doc, record|
-      if doc['primary_type'] == 'transfer'
-        doc['title'] = record['record']['display_string']
-      end
-    }
+      if ['transfer_proposal', 'transfer', 'file_issue_request',
+          'file_issue', 'search_request'].include?(doc['primary_type'])
 
-    indexer.add_document_prepare_hook {|doc, record|
+        doc['title'] = record['record']['display_string'] || record['record']['title']
+
+        doc['map_id_u_sort'] = record['record']['identifier'].to_s.rjust(9, '0')
+        doc['map_id_u_ssort'] = record['record']['identifier']
+        doc['map_agency_id_u_sort'] = record['record']['agency']['_resolved']['qsa_id'].to_s.rjust(9, '0')
+        doc['map_agency_id_u_ssort'] = record['record']['agency']['_resolved']['qsa_id']
+        doc['map_agency_name_u_ssort'] = record['record']['agency']['_resolved']['display_name']['sort_name']
+      end
+
       if doc['primary_type'] == 'transfer_proposal'
-        doc['title'] = record['record']['display_string']
         doc['transfer_status_u_sstr'] = record['record']['status']
         doc['transfer_proposal_title_u_ssort'] = record['record']['title']
-        doc['transfer_proposal_id_u_ssort'] = record['record']['identifier']
         doc['transfer_proposal_status_u_ssort'] = record['record']['status']
-        doc['transfer_proposal_agency_id_u_ssort'] = record['record']['agency']['_resolved']['qsa_id']
-        doc['transfer_proposal_agency_name_u_ssort'] = record['record']['agency']['_resolved']['display_name']['sort_name']
       end
 
       if doc['primary_type'] == 'transfer'
-        doc['title'] = record['record']['display_string']
         doc['transfer_title_u_ssort'] = record['record']['title']
-        doc['transfer_id_u_ssort'] = record['record']['identifier']
         doc['transfer_status_u_ssort'] = record['record']['status']
-        doc['transfer_agency_id_u_ssort'] = record['record']['agency']['_resolved']['qsa_id']
-        doc['transfer_agency_name_u_ssort'] = record['record']['agency']['_resolved']['display_name']['sort_name']
       end
 
       if doc['primary_type'] == 'file_issue_request'
+        # FIRs don't have identifier - sigh
+        doc['map_id_u_sort'] = record['record']['title'].to_s.rjust(9, '0')
+        doc['map_id_u_ssort'] = record['record']['title']
         doc['file_issue_request_draft_u_sbool'] = record['record']['draft']
         doc['file_issue_request_urgent_u_ssort'] = record['record']['urgent'] ? 'z' : 'a'
         doc['file_issue_request_title_u_ssort'] = record['record']['title']
-        doc['file_issue_request_agency_id_u_ssort'] = record['record']['agency']['_resolved']['qsa_id']
-        doc['file_issue_request_agency_name_u_ssort'] = record['record']['agency']['_resolved']['display_name']['sort_name']
       end
 
       if doc['primary_type'] == 'file_issue'
+        # FIs don't have identifier - sigh
+        doc['map_id_u_sort'] = record['record']['title'].to_s.rjust(9, '0')
+        doc['map_id_u_ssort'] = record['record']['title']
         doc['file_issue_urgent_u_ssort'] = record['record']['urgent'] ? 'z' : 'a'
         doc['file_issue_title_u_ssort'] = record['record']['title']
-        doc['file_issue_agency_id_u_ssort'] = record['record']['agency']['_resolved']['qsa_id']
         doc['file_issue_status_u_ssort'] = record['record']['status']
-        doc['file_issue_agency_name_u_ssort'] = record['record']['agency']['_resolved']['display_name']['sort_name']
       end
 
       if doc['primary_type'] == 'search_request'
-        doc['title'] = record['record']['display_string']
         doc['search_request_id_u_ssort'] = record['record']['display_string']
-        doc['search_request_agency_id_u_ssort'] = record['record']['agency']['_resolved']['qsa_id']
         doc['search_request_status_u_ssort'] = record['record']['status']
-        doc['search_request_agency_name_u_ssort'] = record['record']['agency']['_resolved']['display_name']['sort_name']
       end
     }
   end
