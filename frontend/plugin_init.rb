0ccad93c396573bff65dd1ad7dc9199ef8b78767
Payten Giles <payten.giles@gmail.com>
2019-08-07 11:05:16 +1000

    Piv#166921437 expose agency delegates on the agency readonly/edit screens

--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -22,4 +22,57 @@ Rails.application.config.after_initialize do
       }
     )
   )
+
+  class AlwaysReadOnlySection < Plugins::AbstractPluginSection
+    def render_readonly(view_context, record, form_context)
+      view_context.render_aspace_partial(
+        :partial => @erb_template,
+        :locals => {
+          :record => record,
+          :heading_text => @heading_text,
+          :section_id => @section_id ? @section_id : build_section_id(form_context.obj['jsonmodel_type']),
+          :form => form_context,
+        }
+      )
+    end
+
+    def render_edit(view_context, record, form_context)
+      view_context.readonly_context record['jsonmodel_type'], record do |readonly_context|
+        render_readonly(view_context, record, readonly_context)
+      end
+    end
+
+    def supports?(record, mode)
+      return false unless @jsonmodel_types.include?(record['jsonmodel_type'])
+      # don't show on forms for new records 
+      return false if record['uri'].nil? && record['existing_ref'].nil?
+
+      super
+    end
+
+    private
+
+    def parse_opts(opts)
+      super
+
+      @show_on_edit = true
+      @show_on_readonly = true
+      @heading_text = opts.fetch(:heading_text)
+      @erb_template = opts.fetch(:erb_template)
+    end
+  end
+
+  Plugins.register_plugin_section(
+    AlwaysReadOnlySection.new(
+      'as_cartography',
+      'delegates',
+      ['agent_corporate_entity'],
+      {
+        erb_template: 'agents/agency_delegates',
+        heading_text:  I18n.t('agency_delegates._plural'),
+        section_id: "agency_delegates",
+        sidebar_label: I18n.t('agency_delegates._plural'),
+      }
+    )
+  )
 end
