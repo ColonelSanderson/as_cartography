a5e4f56173b8993daa33946c58aa26b1fe9680ee
Mark Triggs <mark@dishevelled.net>
2019-08-08 13:29:07 +1000

    Rejigger repo_id lookups for file issue requests
    We were seeing this old chestnut:
    
         [java] E, [2019-08-08T13:19:43.928071 #19019] ERROR -- : Thread-149566: Unhandled exception!
         [java] E, [2019-08-08T13:19:43.928220 #19019] ERROR -- :
         [java] Missing repo_id for request!
         [java] /mnt/ssd/archivesspace-dev/archivesspace/backend/app/model/ASModel_scoping.rb:203:in `active_repository'
         [java] /mnt/ssd/archivesspace-dev/archivesspace/backend/app/model/ASModel_scoping.rb:144:in `block in set_model_scope'
    
    and we were doing too many DB lookups anyway, so rewrote to be more
    efficient.

--- backend/model/mixins/file_issue_helpers.rb
+++ backend/model/mixins/file_issue_helpers.rb
@@ -8,19 +8,43 @@ module FileIssueHelpers
     # notion of an active repository.  So, we work out the repository that each
     # referenced representation belongs to and work backwards from that.
     def repo_ids_for_items(file_issue_request_items)
-      repo_id_by_item_id = {}
       aspace_models = {}
 
-      file_issue_request_items.values.flatten.each do |item|
+      file_issue_request_items = file_issue_request_items.values.flatten
+
+      # Resolve JSONModel types back to ASModel classes
+      file_issue_request_items.each do |item|
         aspace_record_type = item[:aspace_record_type]
         aspace_record_id = item[:aspace_record_id]
 
         aspace_models[aspace_record_type] ||= ASModel.all_models.find {|m| m.my_jsonmodel(true) && (m.my_jsonmodel(true).record_type == aspace_record_type)}
-
-        repo_id_by_item_id[item[:id]] = aspace_models.fetch(aspace_record_type).any_repo[aspace_record_id][:repo_id]
       end
 
-      repo_id_by_item_id
+      # Load repo_ids of our records
+      repo_id_by_aspace_record = {}
+
+      DB.open do |db|
+        file_issue_request_items.group_by {|item| item[:aspace_record_type]}.each do |group_record_type, item_group|
+          model = aspace_models.fetch(group_record_type)
+
+          # Hit the model table directly to avoid repo scoping constraints
+          db[model.table_name]
+            .filter(:id => item_group.map {|item| item[:aspace_record_id]})
+            .select(:id, :repo_id)
+            .each do |row|
+            key = {:aspace_record_type => group_record_type, :aspace_record_id => row[:id]}
+            repo_id_by_aspace_record[key] = row[:repo_id]
+          end
+        end
+      end
+
+      # Finally, map our items back to the repositories containing them
+      file_issue_request_items.map {|item|
+        key = {:aspace_record_type => item[:aspace_record_type],
+               :aspace_record_id => item[:aspace_record_id]}
+
+        [item[:id], repo_id_by_aspace_record.fetch(key)]
+      }.to_h
     end
   end
 end
